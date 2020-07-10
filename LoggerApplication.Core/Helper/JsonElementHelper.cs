4c47ecfefdc743e0c00d76eec0b035b7aba7bccf
Kürşat Akıncı <kursatakinci.126@gmail.com>
2020-07-10 08:58:43 +0300

    helperların yaratılması

---
+++ LoggerApplication.Core/Helper/JsonElementHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace LoggerApplication.Core.Helper
+{
+    public static class JsonElementHelper
+    {
+        public static string GetString(JsonElement jsonElement)
+        {
+            string json = JsonSerializer.Serialize(jsonElement);
+
+            return json;
+        }
+    }
+}
